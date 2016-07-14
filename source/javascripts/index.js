21a96db26a0f2bf857c0af4bed76aeff3fe046bb
Matthias Feist <matthias.feist@klarna.com>
2016-07-14 09:37:52 +0200

    fixed fontawesome

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -2,7 +2,6 @@ import 'script!jquery'
 import 'script!jquery.scrollto'
 import 'script!lodash'
 import 'foundation-sites'
-import 'font-awesome/css/font-awesome.css'
 $(document).foundation();
 
 var debounced = _.debounce(
