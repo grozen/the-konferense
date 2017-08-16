23c49a90741a6f53dd1c4ea71ed2144baec889f2
Matthias Feist <matthias.feist@klarna.com>
2017-08-16 10:13:00 +0200

    removed sign up link

--- src/view/splash.tsx
+++ src/view/splash.tsx
@@ -42,9 +42,11 @@ export default function splash() : VNode {
       <div className={dateClass}>
         {AppData['The KonferenSE'].date}
       </div>
+      {/*
       <div className={signupClass}>
         <a href='https://goo.gl/forms/1WD8m5g0S0MleRjr1' target='_blank'>sign up now !!!</a>
       </div>
+      */}
     </div>
   )
 }
